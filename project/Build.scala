5fd2ec6faca5211f9a4eb18cecce795f03e62733
Rajiv Nair <rajivrnair@gmail.com>
2012-11-07 16:15:19 +0530

    Added Mongo DB jars

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,15 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+		// Add your project dependencies here,
+		"com.google.code.morphia" % "morphia" % "0.99",
+		"org.mongodb" % "mongo-java-driver" % "2.7.3",
+		"com.google.code.morphia" % "morphia-logging-slf4j" % "0.99"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+		// Add your own project settings here      
+		resolvers += "Maven repository" at "http://morphia.googlecode.com/svn/mavenrepo/",
+    	resolvers += "MongoDb Java Driver Repository" at "http://repo1.maven.org/maven2/org/mongodb/mongo-java-driver/"
     )
-
 }
