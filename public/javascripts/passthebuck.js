1034ce9e6db9d573863adda5eca1d70ea69c78f4
Mahesh Menon <magimenon@gmail.com>
2012-11-08 09:14:28 +0530

    Added overlay for the popup.....

--- public/javascripts/passthebuck.js
+++ public/javascripts/passthebuck.js
@@ -1,11 +1,13 @@
 jQuery(function ($) {
 	jQuery("#team, #category, #task").click(function() {
 		var _id = jQuery(this).attr('id');
+		$('<div id="overlay">').appendTo('body').show();
 		jQuery("."+_id).show();
 	});
 
 	jQuery(".popup-close, .cancel-btn").click(function() {
 		jQuery(".popup-hold").hide();
+		$('#overlay').remove();
 	});
 
 	
