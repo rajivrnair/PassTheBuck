daec7bd9a3091a054901ad76017a2448bc190abb
Mahesh Menon <magimenon@gmail.com>
2012-11-14 11:09:09 +0530

    Removed the form for the edit task and made as a single function for the popup form submit

--- public/javascripts/formhandler.js
+++ public/javascripts/formhandler.js
@@ -14,8 +14,8 @@ jQuery(function ($) {
 		jQuery(".edit-task").show();
 	});
 	
-	jQuery('#teamSubmit').click(function(e) {
-			var formData = $('#formTeam').serialize();
+	jQuery('#teamSubmit, #categorySubmit, #taskSubmit').click(function(e) {
+			var formData = jQuery(this).closest('form').serialize();
 
 		    jsRoutes.controllers.Application.newTeam().ajax({
 		    	data : formData,
@@ -33,37 +33,4 @@ jQuery(function ($) {
 		jQuery(".popup-hold").hide();
 		jQuery('#overlay').hide();
 	};
-
-	jQuery('#categorySubmit').click(function(e) {
-			var formData = $('#formCategory').serialize();
-
-		    jsRoutes.controllers.Application.newCategory().ajax({
-		    	data : formData,
-		        success : function(data) {
-		            jQuery(".popup-hold").hide();
-		            jQuery('#overlay').hide();
-		        },
-		        error : function(data) {
-		        	alert("Mayire.. form submit cheyyada!");
-		        }
-		    });
-		    return false;
-		});
-
-	jQuery('#taskSubmit').click(function(e) {
-			var formData = $('#formTask').serialize();
-
-		    jsRoutes.controllers.Application.newTask().ajax({
-		    	data : formData,
-		        success : function(data) {
-		            jQuery(".popup-hold").hide();
-		            jQuery('#overlay').hide();
-		        },
-		        error : function(data) {
-		        	alert("Mayire.. form submit cheyyada!");
-		        }
-		    });
-		    return false;
-		});
-
 });
