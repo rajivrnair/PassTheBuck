e8329f096b146a98080af65ca72534ba66680e05
Mahesh Menon <magimenon@gmail.com>
2012-11-14 07:23:53 +0530

    Cleaned up the code and added first cut of "Edit - Task" functionality.

--- public/javascripts/formhandler.js
+++ public/javascripts/formhandler.js
@@ -6,26 +6,33 @@ jQuery(function ($) {
 	});
 
 	jQuery(".popup-close, .cancel-btn").click(function() {
-		jQuery(".popup-hold").hide();
-		jQuery('#overlay').hide();
+		hideForm();
+	});
+
+	jQuery("ul#listings li").click(function() {
+		jQuery("#overlay").show();
+		jQuery(".edit-task").show();
 	});
 	
-
 	jQuery('#teamSubmit').click(function(e) {
 			var formData = $('#formTeam').serialize();
 
 		    jsRoutes.controllers.Application.newTeam().ajax({
 		    	data : formData,
 		        success : function(data) {
-		            jQuery(".popup-hold").hide();
-		            jQuery('#overlay').hide();
+		            hideForm();
 		        },
 		        error : function(data) {
 		        	alert("Mayire.. form submit cheyyada!");
 		        }
 		    });
 		    return false;
-		});
+	});
+
+	var hideForm = function() {
+		jQuery(".popup-hold").hide();
+		jQuery('#overlay').hide();
+	};
 
 	jQuery('#categorySubmit').click(function(e) {
 			var formData = $('#formCategory').serialize();
