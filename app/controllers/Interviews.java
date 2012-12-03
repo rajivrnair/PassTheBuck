b040277123456d21867de8af6d339be79b3c3c8b
Rajiv Nair <rajivrnair@gmail.com>
2012-12-03 11:54:02 +0530

    Added missing Iterviews controller.

---
+++ app/controllers/Interviews.java
@@ -0,0 +1,48 @@
+package controllers;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import models.Category;
+import models.Interview;
+import models.Team;
+import play.data.Form;
+import play.data.format.Formatters;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Interviews extends Controller {
+
+	static Form<Team> teamForm = form(Team.class);
+	static Form<Category> categoryForm = form(Category.class);
+	static Form<Interview> interviewForm = form(Interview.class);
+
+	public static Result newInterview() {
+		Formatters.register(Category.class, new Formatters.SimpleFormatter<Category>() {
+
+			@Override
+			public Category parse(String input, Locale locale)
+					throws ParseException {
+				Category byId = Category.find.byId(new Long(input));
+				return byId;
+			}
+
+			@Override
+			public String print(Category cat, Locale locale) {
+				return cat.id + ":" + cat.name;
+			}
+		});
+		
+		Form<Interview> filledForm = interviewForm.bindFromRequest();
+		if (filledForm.hasErrors()) {
+			return badRequest(views.html.index.render(Team.all(), teamForm, Category.all(), categoryForm, Interview.all(), filledForm));
+		} else {
+			filledForm.get().save();
+			return redirect(routes.Application.index());
+		}
+	}
+
+	public static Result deleteInterview(Long id) {
+		return TODO;
+	}
+}
