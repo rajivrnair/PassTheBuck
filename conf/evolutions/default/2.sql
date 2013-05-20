d9556b22721b5a3397b5914dbea1fef87341c36c
Rajiv Nair <rajivrnair@gmail.com>
2013-05-20 18:22:44 +0530

    updated bootstrap db script

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -12,8 +12,8 @@ insert into category(id, name, duration_in_minutes, credits) values (category_se
 insert into category(id, name, duration_in_minutes, credits) values (category_seq.nextval, 'II Round', 90, 30);
 
 -- Create User
-insert into user (id, external_identity, email, name, locale, picture) values (1,'101491561765242970579','rajivrnair@gmail.com','Rajiv Nair','en_GB','https://lh6.googleusercontent.com/-gFZtm3h0sy4/AAAAAAAAAAI/AAAAAAAAD5s/-Lj2Sm0rF8k/photo.jpg');
-insert into user (id, external_identity, email, name, locale, picture) values (2,'101866479603323534663','srininara@gmail.com','Srinivas Narayanan','en','https://lh5.googleusercontent.com/-nUWeFiZbJdQ/AAAAAAAAAAI/AAAAAAAABNo/BsdlLR5h57M/photo.jpg');
+insert into user (id, external_identity, email, name, locale, picture) values (-1,'101491561765242970579','rajivrnair@gmail.com','Rajiv Nair','en_GB','https://lh6.googleusercontent.com/-gFZtm3h0sy4/AAAAAAAAAAI/AAAAAAAAD5s/-Lj2Sm0rF8k/photo.jpg');
+insert into user (id, external_identity, email, name, locale, picture) values (-2,'101866479603323534663','srininara@gmail.com','Srinivas Narayanan','en','https://lh5.googleusercontent.com/-nUWeFiZbJdQ/AAAAAAAAAAI/AAAAAAAABNo/BsdlLR5h57M/photo.jpg');
 
 # --- !Downs
 delete * from team;
